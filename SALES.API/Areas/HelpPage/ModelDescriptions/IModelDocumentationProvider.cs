ce5f5ff2c3419b70cca7661e9d3f50a371ecc980
acarrales25 <46935830+acarrales25@users.noreply.github.com>
2019-01-23 15:01:48 -0600

    Sales20190123

---
+++ SALES.API/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace SALES.API.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ SALES.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SALES.API.WebApiApplication" Language="C#" %>
